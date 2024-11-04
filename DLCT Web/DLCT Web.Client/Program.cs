e60ee2a7c311a6027a312a5038793f92cbf0c150
Roy <roynoten@hotmail.com>
2024-11-04 12:15:25 +0100

    Initial setup, broken version

--- DLCT Web/DLCT Web.Client/Program.cs
+++ DLCT Web/DLCT Web.Client/Program.cs
@@ -1,5 +1,8 @@
+using DLCT_Web.Client;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+builder.Services.AddScoped<WebParser>();
 
 await builder.Build().RunAsync();
